dfc6ebc93880427113cff2f1917805e0926a928c
HAPPYCAT <charlottechoibk@gmail.com>
2025-07-06 21:19:38 +0800

    extract_all_data_from_1_site

--- script.py
+++ script.py
@@ -22,11 +22,22 @@ if response.status_code == 200:
     iap_headers = soup.find_all(string=re.compile(r'\$\d+(\.\d{2})?'))
     cnt = len(iap_headers)
     print(f"Found {cnt} elements with $ sign.")
+    results = []
 
     for i, price in enumerate(iap_headers):
         if price.parent.name == "span":
-            print(f"\nPrice {i+1}:")
-            print("Text:", price)
+            parent = price.parent
+            block_tag = parent.find_parent()
+            spans = block_tag.find_all('span')
+                # The last one is the price, so the rest is the name
+            if len(spans) >= 2:
+                name_span = spans[0]  # or sometimes more robust to do: pick the span that is not the price span
+                results.append((name_span.text.strip(), price.strip()))
+
+    i = 1
+    for name, price in results:
+        print(f"{i}: {name} | Price: {price}")
+        i += 1
 
 else:
     print("Failed to retrieve the page:", response.status_code)
